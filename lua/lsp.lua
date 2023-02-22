a1df9ac2f3116e0c9d686a5e5e431812b360f889
jamesjjx <jamesjjx@laptop>
2023-02-21 20:13:51 -0800

    Remove unused files.

--- lua/lsp.lua
+++
@@ -1,6 +0,0 @@
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-  -- delay update diagnostics
-  update_in_insert = false,
-})
-
-require("configs.sumneko_lua")
