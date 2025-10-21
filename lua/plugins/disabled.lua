3e127fe8539dccaba0c01e77b74f05748c88a548
jamesjjx <jamesjjx@laptop>
2025-10-20 19:11:29 -0700

    update a few renamed plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
-  { "echasnovski/mini.indentscope", enabled = false },
+  { "nvim-mini/mini.indentscope", enabled = false },
   { "lewis6991/gitsigns.nvim", enabled = false },
   { "folke/flash.nvim", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
 }
